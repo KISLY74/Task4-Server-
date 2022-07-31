82117a511cae0878846400beab2c9d9cf2efc760
KISLY74 <kisly.nikita@mail.ru>
2022-07-31 18:45:14 +0300

    feat: call two methods for connect and synchronization with datebase of postgresql

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ const express = require('express')
 const PORT = process.env.PORT || 8080
 const app = express()
 const cors = require('cors')
+const sequelize = require('./db')
 
 app.use(cors())
 app.use(express.json())
 
 const start = async () => {
   try {
+    await sequelize.authenticate()
+    await sequelize.sync()
     app.listen(PORT, () => console.log(`server started on PORT ${PORT}`))
   }
   catch (e) {
