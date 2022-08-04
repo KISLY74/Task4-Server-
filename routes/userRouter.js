e79bb292ca82e32567678799295f496590915a0c
KISLY74 <kisly.nikita@mail.ru>
2022-08-04 19:43:48 +0300

    feat: set routes and his complete methods

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -6,5 +6,10 @@ const authMiddleware = require('../middleware/authMiddleware')
 router.post('/regin', userController.regin)
 router.post('/login', userController.login)
 router.get('/auth', authMiddleware, userController.check)
+router.get('/all', userController.getUsers)
+router.post('/get', userController.getOneUser)
+router.put('/change/status', userController.changeStatus)
+router.get('/status', userController.getCountUsersStatus)
+router.put('/change/dateLogin', userController.changeDateLogin)
 
 module.exports = router
