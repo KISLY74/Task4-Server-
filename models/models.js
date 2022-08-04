982692f9c51394a14440323b59a6e4fe9f6f1306
KISLY74 <kisly.nikita@mail.ru>
2022-08-04 19:44:23 +0300

    fix: remove unique filed for email

--- models/models.js
+++ models/models.js
@@ -3,7 +3,7 @@ const { DataTypes } = require('sequelize')
 
 const User = sequelize.define('user', {
   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-  email: { type: DataTypes.STRING, unique: true },
+  email: { type: DataTypes.STRING },
   password: { type: DataTypes.STRING },
   name: { type: DataTypes.STRING },
   dateRegin: { type: DataTypes.DATE },
