a0d8c6fc1f9db1bdf572f71633f39b3913bf0773
KISLY74 <kisly.nikita@mail.ru>
2022-08-08 14:42:53 +0300

    feat: change return errors with res.status, add methods of get users, get one user by email, check user is authorizated, change status user, get count status users for progress bar, change date login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,3 @@
-const ApiError = require('../error/ApiError')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const User = require('../models/models')
@@ -12,36 +11,79 @@ const generateJwt = (id, email) => {
 }
 
 class UserController {
-  async regin(req, res, next) {
+  async regin(req, res) {
     const { email, password, name } = req.body
     if (!email || !password) {
-      return next(ApiError.badRequest('Неккоректный email или password'))
+      return res.status(404).json({ message: "Неккоректный email или password" })
     }
-    const candidate = await User.findOne({ where: { email } })
-    if (candidate) {
-      return next(ApiError.badRequest('Пользователь с таким email уже существует'))
+    const candidates = await User.findAll({ where: { email: email } })
+    if (candidates.length !== 0) {
+      if (candidates.some(e => e.status !== "Delete")) {
+        return res.status(404).json({ message: "Пользователь с таким email уже существует" })
+      }
     }
     const hashPassword = await bcrypt.hash(password, 5)
-    const user = await User.create({ email, password: hashPassword, name: name, status: "Unblock", dateRegin: new Date(Date.now()).toISOString().slice(0, 10) })
-    const token = generateJwt(user.id, user.email)
+    const user = await User.create({ email, password: hashPassword, name: name, status: "Unblock", dateRegin: new Date(Date.now()).toISOString().slice(0, 10) }).catch((err) => console.log(err))
+    const token = generateJwt(user.id, user.email, user.name)
     return res.json({ token })
   }
-  async login(req, res, next) {
+  async login(req, res) {
     const { email, password } = req.body
     const user = await User.findOne({ where: { email } })
     if (!user) {
-      return next(ApiError.internal('Пользователь не найден'))
+      return res.status(500).json({ message: "Пользователь не найден" })
     }
     const comparePassword = bcrypt.compareSync(password, user.password)
     if (!comparePassword) {
-      return next(ApiError.internal('Указан неверный пароль'))
+      return res.status(500).json({ message: "Указан неверный пароль" })
     }
-    const token = generateJwt(user.id, user.email)
+    const token = generateJwt(user.id, user.email, user.name)
     return res.json({ token })
   }
-  async check(req, res, next) {
-    const token = generateJwt(req.user.id, req.user.email)
+  async check(req, res) {
+    const token = generateJwt(req.user.id, req.user.email, req.user.name)
     return res.json({ token })
+  }
+  async getUsers(req, res) {
+    const users = await User.findAll({ order: [['id', 'ASC'],] })
+    return res.json(users)
+  }
+  async getOneUser(req, res) {
+    const users = await User.findAll({ where: { email: req.body.email } })
+    for (let i = 0; i < users.length; i++) {
+      if (users[i].status !== "Delete") {
+        return res.json(users[i])
+      }
+    }
+    return res.json(null)
+  }
+  async changeStatus(req, res) {
+    const user = await User.update(
+      {
+        status: req.body.status,
+      },
+      {
+        where: { id: req.body.id },
+      }
+    );
+    return res.json(user)
+  }
+  async getCountUsersStatus(req, res) {
+    const blockUsers = await User.findAll({ where: { status: "Block" } })
+    const deleteUsers = await User.findAll({ where: { status: "Delete" } })
+    const unblockUsers = await User.findAll({ where: { status: "Unblock" } })
+    return res.json({ block: blockUsers.length, delete: deleteUsers.length, unblock: unblockUsers.length })
+  }
+  async changeDateLogin(req, res) {
+    const user = await User.update(
+      {
+        dateLastLogin: new Date(Date.now()).toISOString().slice(0, 10),
+      },
+      {
+        where: { email: req.body.email },
+      }
+    );
+    return res.json(user)
   }
 }
 
