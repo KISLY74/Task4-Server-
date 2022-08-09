d4b46af32d836f69de5907958f1ef094b775240d
KISLY74 <kisly.nikita@mail.ru>
2022-08-09 13:32:10 +0300

    fix: change type on string and remove field versionKey

--- models/models.js
+++ models/models.js
@@ -1,13 +1,15 @@
 const { Schema, model } = require('mongoose')
 
 const User = new Schema({
-  id: { type: Number, unique: true, autoIncrement: true, required: true },
+  id: { type: String, unique: true, require: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
   name: { type: String, required: true },
-  dateRegin: { type: Date, required: true },
-  dateLastLogin: { type: Date },
+  dateRegin: { type: String, required: true },
+  dateLastLogin: { type: String },
   status: { type: String, required: true },
+}, {
+  versionKey: false
 })
 
 module.exports = new model('User', User)
