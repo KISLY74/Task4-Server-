9af11e2a4bd49d04f90eab08894fa623853d1342
KISLY74 <kisly.nikita@mail.ru>
2022-08-09 13:35:56 +0300

    fix: change some methods on methods of mongoose, change arg id on _id, add condition in login where find user and condition of name in regin

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,31 +5,33 @@ const User = require('../models/models')
 const generateJwt = (id, email) => {
   return jwt.sign(
     { id, email },
-    process.env.SECRET_KEY,
+    "0123dfrc",
     { expiresIn: "24h" }
   )
 }
 
 class UserController {
   async regin(req, res) {
-    const { email, password, name } = req.body
-    if (!email || !password) {
-      return res.status(404).json({ message: "Неккоректный email или password" })
+    let { email, password, name } = req.body
+    if (!email || !password || !name) {
+      return res.status(404).json({ message: "Заполните пустые поля" })
     }
-    const candidates = await User.findAll({ where: { email: email } })
-    if (candidates.length !== 0) {
-      if (candidates.some(e => e.status !== "Delete")) {
+    const candidate = await User.findOne({ email: email })
+    if (candidate !== null) {
+      if (candidate.status !== "Delete") {
         return res.status(404).json({ message: "Пользователь с таким email уже существует" })
       }
     }
-    const hashPassword = await bcrypt.hash(password, 5)
-    const user = await User.create({ email, password: hashPassword, name: name, status: "Unblock", dateRegin: new Date(Date.now()).toISOString().slice(0, 10) }).catch((err) => console.log(err))
-    const token = generateJwt(user.id, user.email, user.name)
+    password = password.toString()
+    const hashPassword = await bcrypt.hash(password.toString(), 5)
+    const user = new User({ email: email, password: hashPassword, name: name, dateRegin: new Date(Date.now()).toISOString(), dateLastLogin: "None", status: "Unblock" })
+    await user.save()
+    const token = generateJwt(user._id, user.email, user.name)
     return res.json({ token })
   }
   async login(req, res) {
     const { email, password } = req.body
-    const user = await User.findOne({ where: { email } })
+    const user = await User.findOne({ email: email, status: { $ne: "Delete" } })
     if (!user) {
       return res.status(500).json({ message: "Пользователь не найден" })
     }
@@ -37,19 +39,19 @@ class UserController {
     if (!comparePassword) {
       return res.status(500).json({ message: "Указан неверный пароль" })
     }
-    const token = generateJwt(user.id, user.email, user.name)
+    const token = generateJwt(user._id, user.email, user.name)
     return res.json({ token })
   }
   async check(req, res) {
-    const token = generateJwt(req.user.id, req.user.email, req.user.name)
+    const token = generateJwt(req.user._id, req.user.email, req.user.name)
     return res.json({ token })
   }
   async getUsers(req, res) {
-    const users = await User.findAll({ order: [['id', 'ASC'],] })
+    const users = await User.find()
     return res.json(users)
   }
   async getOneUser(req, res) {
-    const users = await User.findAll({ where: { email: req.body.email } })
+    const users = await User.find({ email: req.body.email })
     for (let i = 0; i < users.length; i++) {
       if (users[i].status !== "Delete") {
         return res.json(users[i])
@@ -58,30 +60,20 @@ class UserController {
     return res.json(null)
   }
   async changeStatus(req, res) {
-    const user = await User.update(
-      {
-        status: req.body.status,
-      },
-      {
-        where: { id: req.body.id },
-      }
-    );
+    const user = await User.findOneAndUpdate({ _id: req.body.id }, { status: req.body.status })
     return res.json(user)
   }
   async getCountUsersStatus(req, res) {
-    const blockUsers = await User.findAll({ where: { status: "Block" } })
-    const deleteUsers = await User.findAll({ where: { status: "Delete" } })
-    const unblockUsers = await User.findAll({ where: { status: "Unblock" } })
+    const blockUsers = await User.find({ status: "Block" })
+    const deleteUsers = await User.find({ status: "Delete" })
+    const unblockUsers = await User.find({ status: "Unblock" })
     return res.json({ block: blockUsers.length, delete: deleteUsers.length, unblock: unblockUsers.length })
   }
   async changeDateLogin(req, res) {
-    const user = await User.update(
-      {
-        dateLastLogin: new Date(Date.now()).toISOString().slice(0, 10),
-      },
-      {
-        where: { email: req.body.email },
-      }
+    const user = await User.findOneAndUpdate(
+      { _id: req.body.id }, {
+      dateLastLogin: new Date(Date.now()).toISOString(),
+    }
     );
     return res.json(user)
   }
